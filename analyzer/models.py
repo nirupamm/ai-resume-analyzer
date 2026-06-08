f6cc663e4a3ce09dae99ab107ae9b5470ba8bb8a
Nirupam Aryal <nirupam62542@gmail.com>
2026-06-08 12:24:50 +0545

    Add resume analysis persistence model

--- analyzer/models.py
+++ analyzer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class ResumeAnalysis(models.Model):
+    filename = models.CharField(max_length=255)
+    score = models.IntegerField(null=True, blank=True)
+    analysis = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.filename
